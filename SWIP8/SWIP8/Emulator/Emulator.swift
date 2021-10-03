002c5df878de255c963b704ec7fb839509fdedea
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-10-03 20:18:37 +0300

    Reset all fields. Minor refactoring.

--- SWIP8/SWIP8/Emulator/Emulator.swift
+++ SWIP8/SWIP8/Emulator/Emulator.swift
@@ -63,18 +63,40 @@ class Emulator {
 	private static let ResolutionWidth: UInt16 = 64
 	
 
-	private var memory = Array<UInt8>(repeating: 0, count: Int(MemorySize))
-	private var display = Array<UInt8>(repeating: 0, count: Int(ResolutionWidth * ResolutionHeight))
-	private var registers = Array<UInt8>(repeating: 0, count: RegisterCount)
-	private var programCounter: UInt16 = ReservedMemorySize
-	private var indexRegister: UInt16 = 0
-	private var stack = Array<UInt16>()
-	private var delayTimer: UInt8 = 0
-	private var soundTimer: UInt8 = 0
-	private var quit = false
+	private (set) var memory = Array<UInt8>(repeating: 0, count: Int(MemorySize))
+	private (set) var display = Array<UInt8>(repeating: 0, count: Int(ResolutionWidth * ResolutionHeight))
+	private (set) var registers = Array<UInt8>(repeating: 0, count: RegisterCount)
+	private (set) var programCounter: UInt16 = ReservedMemorySize
+	private (set) var indexRegister: UInt16 = ReservedMemorySize
+	private (set) var stack = Array<UInt16>()
+	private (set) var delayTimer: UInt8 = 0
+	private (set) var soundTimer: UInt8 = 0
+	private (set) var quit = false
 	
 	init() {
 		
+	}
+	
+	func reset() {
+		quit = true
+		indexRegister = Self.ReservedMemorySize
+		programCounter = Self.ReservedMemorySize
+		
+		clearScreen()
+		registers.withUnsafeMutableBytes { ptr in
+			_ = memset(ptr.baseAddress, 0, ptr.count)
+		}
+		memory.withUnsafeMutableBytes { ptr in
+			_ = memset(ptr.baseAddress, 0, ptr.count)
+		}
+		memory.replaceSubrange(
+			Int(Self.FontDataOffset)..<Int(Self.FontDataOffset) + Self.FontData.count,
+			with: Self.FontData
+		)
+		stack.removeAll(keepingCapacity: true)
+		
+		delayTimer = 0
+		soundTimer = 0
 	}
 	
 	func load(rom: [UInt8]) {
@@ -92,11 +114,99 @@ class Emulator {
 		}
 	}
 	
+	func execute(instruction: Instruction) {
+		switch instruction.o {
+		case 0x00 where instruction.b == 0xe0:
+			clearScreen()
+		case 0x00 where instruction.b == 0xee:
+			if let returnTo = stack.popLast() {
+				programCounter = returnTo
+			} else {
+				quit = true
+			}
+		case 0x00:
+			fatalError("Jump to machine code isn't supported")
+		case 0x01:
+			programCounter = instruction.nnn
+		case 0x02:
+			// TODO: prevent stack from growing too tall
+			stack.append(programCounter)
+			programCounter = instruction.nnn
+		case 0x03:
+			if registers[instruction.x] == instruction.b {
+				programCounter += 2
+			}
+		case 0x04:
+			if registers[instruction.x] != instruction.b {
+				programCounter += 2
+			}
+		case 0x05:
+			if registers[instruction.x] == registers[instruction.y] {
+				programCounter += 2
+			}
+		case 0x06:
+			registers[instruction.x] = instruction.b
+		case 0x07:
+			registers[instruction.x] &+= instruction.b
+		case 0x08:
+			fatalError("Boolean and arithmetic operations aren't implemented")
+		case 0x09:
+			if registers[instruction.x] == registers[instruction.y] {
+				programCounter += 2
+			}
+		case 0x0a:
+			indexRegister = instruction.nnn
+		case 0x0b:
+			// TODO: implement an option to increment by registers[x]
+			programCounter = instruction.nnn + registers[0]
+		case 0x0c:
+			registers[instruction.x] = UInt8.random(in: 0..<UInt8.max) & instruction.b
+		case 0x0d:
+			fatalError("Draw call isn't implemented")
+		case 0x0e where instruction.b == 0x9e:
+			fatalError("Skip if key pressed isn't implemented")
+		case 0x0e where instruction.b == 0xa1:
+			fatalError("Skip if key not pressed isn't implemented")
+		case 0x0f where instruction.b == 0x07:
+			registers[instruction.x] = delayTimer
+		case 0x0f where instruction.b == 0x15:
+			delayTimer = registers[instruction.x]
+		case 0x0f where instruction.b == 0x18:
+			soundTimer = registers[instruction.x]
+		case 0x0f where instruction.b == 0x1e:
+			indexRegister += UInt16(registers[instruction.x])
+			if indexRegister >= 0x1000 {
+				indexRegister &= 0x0fff
+				// TODO: implement an option to skip flagging this overlow
+				registers[0x0f] = 1
+			}
+		case 0x0f where instruction.b == 0x0a:
+			fatalError("Get key instruction isn't implemented")
+		case 0x0f where instruction.b == 0x29:
+			indexRegister = Self.FontDataOffset + UInt16(registers[instruction.x] & 0x0f)
+		case 0x0f where instruction.b == 0x33:
+			let num = registers[instruction.x]
+			memory[indexRegister] = num / 100
+			memory[indexRegister + 1] = num / 10 % 10
+			memory[indexRegister + 2] = num % 10
+		case 0x0f where instruction.b == 0x55:
+			// TODO: implement an option to increment/decrement index register
+			for i in 0...instruction.x {
+				memory[indexRegister + i] = registers[i]
+			}
+		case 0x0f where instruction.b == 0x65:
+			for i in 0...instruction.x {
+				registers[i] = memory[indexRegister + i]
+			}
+		default:
+			fatalError("Unknown instruction: \(instruction)")
+		}
+	}
+
 	// TODO: throw errors instead of crashing
 	func run() {
-		var i = 0
+		quit = false
 		while !quit {
-			i += 1
 			let instruction = Instruction(a: memory[programCounter], b: memory[programCounter + 1])
 			programCounter += 2
 			
@@ -104,104 +214,8 @@ class Emulator {
 				fatalError("PC (\(programCounter)) indexes out of memory.")
 			}
 			
-			switch instruction.o {
-			case 0x00 where instruction.b == 0xe0:
-				clearScreen()
-			case 0x00 where instruction.b == 0xee:
-				if let returnTo = stack.popLast() {
-					programCounter = returnTo
-				} else {
-					quit = true
-				}
-			case 0x00:
-				fatalError("Jump to machine code isn't supported")
-			case 0x01:
-				programCounter = instruction.nnn
-			case 0x02:
-				// TODO: prevent stack from growing too tall
-				stack.append(programCounter)
-				programCounter = instruction.nnn
-			case 0x03:
-				if registers[instruction.x] == instruction.b {
-					programCounter += 2
-				}
-			case 0x04:
-				if registers[instruction.x] != instruction.b {
-					programCounter += 2
-				}
-			case 0x05:
-				if registers[instruction.x] == registers[instruction.y] {
-					programCounter += 2
-				}
-			case 0x06:
-				registers[instruction.x] = instruction.b
-			case 0x07:
-				registers[instruction.x] &+= instruction.b
-			case 0x08:
-				fatalError("Boolean and arithmetic operations aren't implemented")
-			case 0x09:
-				if registers[instruction.x] == registers[instruction.y] {
-					programCounter += 2
-				}
-			case 0x0a:
-				indexRegister = instruction.nnn
-			case 0x0b:
-				// TODO: implement an option to increment by registers[x]
-				programCounter = instruction.nnn + registers[0]
-			case 0x0c:
-				registers[instruction.x] = UInt8.random(in: 0..<UInt8.max) & instruction.b
-			case 0x0d:
-				fatalError("Draw call isn't implemented")
-			case 0x0e where instruction.b == 0x9e:
-				fatalError("Skip if key pressed isn't implemented")
-			case 0x0e where instruction.b == 0xa1:
-				fatalError("Skip if key not pressed isn't implemented")
-			case 0x0f where instruction.b == 0x07:
-				registers[instruction.x] = delayTimer
-			case 0x0f where instruction.b == 0x15:
-				delayTimer = registers[instruction.x]
-			case 0x0f where instruction.b == 0x18:
-				soundTimer = registers[instruction.x]
-			case 0x0f where instruction.b == 0x1e:
-				indexRegister += UInt16(registers[instruction.x])
-				if indexRegister >= 0x1000 {
-					indexRegister &= 0x0fff
-					// TODO: implement an option to skip flagging this overlow
-					registers[0x0f] = 1
-				}
-			case 0x0f where instruction.b == 0x0a:
-				fatalError("Get key instruction isn't implemented")
-			case 0x0f where instruction.b == 0x29:
-				indexRegister = Self.FontDataOffset + UInt16(registers[instruction.x] & 0x0f)
-			case 0x0f where instruction.b == 0x33:
-				let num = registers[instruction.x]
-				memory[indexRegister] = num / 100
-				memory[indexRegister + 1] = num / 10 % 10
-				memory[indexRegister + 2] = num % 10
-			case 0x0f where instruction.b == 0x55:
-				// TODO: implement an option to increment/decrement index register
-				for i in 0...instruction.x {
-					memory[indexRegister + i] = registers[i]
-				}
-			case 0x0f where instruction.b == 0x65:
-				for i in 0...instruction.x {
-					registers[i] = memory[indexRegister + i]
-				}
-			default:
-				fatalError("Unknown instruction: \(instruction)")
-			}
+			execute(instruction: instruction)
 		}
-	}
-	
-	private func reset() {
-		programCounter = Self.ReservedMemorySize
-		memory.withUnsafeMutableBytes { ptr in
-			_ = memset(ptr.baseAddress, 0, ptr.count)
-		}
-		memory.replaceSubrange(
-			Int(Self.FontDataOffset)..<Int(Self.FontDataOffset) + Self.FontData.count,
-			with: Self.FontData
-		)
 	}
 	
 	private func clearScreen() {
