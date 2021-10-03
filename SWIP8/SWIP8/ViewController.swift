d5a5eddcbe7a4c53941f1abe64cb8347182ed416
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-10-03 14:21:20 +0300

    Project start.

---
+++ SWIP8/SWIP8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SWIP8
+//
+//  Created by Dmitriy Shmilo on 03.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
