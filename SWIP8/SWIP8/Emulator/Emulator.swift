23a4f3c3a2824170befbe293285d778a0a4e877d
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-10-07 11:24:37 +0300

    Fix SwiftLint empty_count error.

--- SWIP8/SWIP8/Emulator/Emulator.swift
+++ SWIP8/SWIP8/Emulator/Emulator.swift
@@ -88,7 +88,7 @@ class Emulator {
 	func load(rom: [UInt8]) throws {
 		// each instruction is two bytes in length
 		// TODO: There might be an odd number of data bytes, need to verify with the spec
-		guard rom.count.isMultiple(of: 2) && rom.count > 0 else {
+		guard rom.count.isMultiple(of: 2) && !rom.isEmpty else {
 			throw LoadError.InvalidInputLength
 		}
 		
